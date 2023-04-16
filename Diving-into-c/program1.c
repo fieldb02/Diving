d0c7ce8ebc37ec897c91801be5dd5a2bcf24fd1f
fieldb02 <bonyango118@gmail.com>
2023-04-16 19:29:35 +0300

    Success

---
+++ Diving-into-c/program1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("My first c program\n");
+	printf("Pretty, cool!\n");
+	exit(0);
+}
